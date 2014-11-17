030b68d39feefbff6ee18eb39490c8457e0aa385
RomanDubinin <Roman230394@yandex.ru>
2014-11-18 03:01:51 +0500

    немного красивее

--- FuncGraphic/FuncGraphic/Program.cs
+++ FuncGraphic/FuncGraphic/Program.cs
@@ -7,116 +7,119 @@ using System.Drawing;
 
 namespace FuncGraphic
 {
-    class Program
-    {
-        const int WindowSize = 500;
-        const int XFrom = -1;
-        const int XTo = 1;
+	class Program
+	{
+		const int WindowSize = 500;
+		const int XFrom = -1;
+		const int XTo = 1;
+		private static Pen pen = new Pen(Color.Blue, 1);
+		private static Pen axisPen = new Pen(Color.Black, 2);
+		private static Graphics g;
 
-        private delegate double FuncToShow(double x);
+		private delegate double FuncToShow(double x);
 
-        private static FuncToShow myF = (x) => x*x;
+		private static FuncToShow myF = Cos1DivX;
 
-        private static double Cos1DivX(double x)
-        {
-            return Math.Cos(1 / x);
-        }
+		private static double Cos1DivX(double x)
+		{
+			return Math.Cos(1 / x);
+		}
 
-        private static double XCube(double x)
-        {
-            return x*x*x;
-        }
+		private static double XCube(double x)
+		{
+			return x * x * x;
+		}
 
-        private static double XIn5(double x)
-        {
-            return x*x*x*x*x;
-        }
+		private static double XIn5(double x)
+		{
+			return x * x * x * x * x;
+		}
 
-        private static Tuple<double, double> GetMinMaxValues(FuncToShow F, double xFrom, double xTo)
-        {
-            double maxY = F(XFrom);
-            double minY = F(XFrom);
-            int screenX;
-            double x, y;
-            for (screenX = 0; screenX < WindowSize; ++screenX)
-            {
-                x = (XFrom + screenX * (xTo - XFrom) / WindowSize);
-                y = F(x);
-                if (y < minY)
-                    minY = y;
-                if (y > maxY)
-                    maxY = y;
-            }
-            return Tuple.Create(minY, maxY);
-        }
+		private static Tuple<double, double> GetMinMaxValues(FuncToShow F, double xFrom, double xTo)
+		{
+			double maxY = F(XFrom);
+			double minY = F(XFrom);
+			int screenX;
+			double x, y;
+			for (screenX = 0; screenX < WindowSize; ++screenX)
+			{
+				x = (XFrom + screenX * (xTo - XFrom) / WindowSize);
+				y = F(x);
+				if (y < minY)
+					minY = y;
+				if (y > maxY)
+					maxY = y;
+			}
+			return Tuple.Create(minY, maxY);
+		}
 
-        private static void CreateImage(Bitmap image, FuncToShow F, double xFrom, double xTo)
-        {
-            Pen pen = new Pen(Color.Blue, 1);
-            Pen axisPen = new Pen(Color.Black, 2);
-            Graphics g = Graphics.FromImage(image);
+		private static void DrowAxis(double minX, double maxX, double minY, double maxY)
+		{
+			if (minX < 0 && maxX > 0)
+			{
+				int screenX0 = (int)(-minX * WindowSize / (maxX - minX));
+				g.DrawLine(axisPen, screenX0, WindowSize, screenX0, 0);
+			}
 
-            Tuple<double, double> minMax = GetMinMaxValues(F, XFrom, XTo);
-            double minY = minMax.Item1;
-            double maxY = minMax.Item2;
+			if (minY < 0 && maxY > 0)
+			{
+				int screenY0 = (int)(-minY * WindowSize / (maxY - minY));
+				g.DrawLine(axisPen, 0, screenY0, WindowSize, screenY0);
+			}
+		}
 
-            if (XFrom < 0 && XTo > 0)
-            {
-                int screenX0 = (int)(-XFrom * WindowSize / (xTo - XFrom));
-                g.DrawLine(axisPen, screenX0, WindowSize, screenX0, 0);
-            }
+		private static void CreateImage(Bitmap image, FuncToShow F, double xFrom, double xTo)
+		{
 
-            if (minY < 0 && maxY > 0)
-            {
-                int screenY0 = (int)(-minY * WindowSize / (maxY - minY));
-                g.DrawLine(axisPen, 0, screenY0, WindowSize, screenY0);
-            }
-                
+			Tuple<double, double> minMax = GetMinMaxValues(F, XFrom, XTo);
+			double minY = minMax.Item1;
+			double maxY = minMax.Item2;
 
-            Point nextPoint, oldPoint;
-            int screenX, screenY;
-            double x, y;
+			DrowAxis(XFrom, XTo, minY, maxY);
 
-            screenY = (int)((F(XFrom) - minY) * WindowSize / (maxY - minY));
-            oldPoint = new Point(0, screenY);
+			Point nextPoint, oldPoint;
+			int screenX, screenY;
+			double x, y;
 
-            for (screenX = 0; screenX < WindowSize; screenX++)
-            {
-                x = (XFrom + screenX * (xTo - XFrom) / WindowSize);
-                y = F(x);
-                if (double.IsNaN(y))
-                    continue;
-                screenY = (int)((maxY - y) * WindowSize / (maxY - minY));
-                nextPoint = new Point(screenX, screenY);
-                g.DrawLine(pen, oldPoint, nextPoint);
-                oldPoint = nextPoint;
-            }
-        }
+			screenY = (int)((F(XFrom) - minY) * WindowSize / (maxY - minY));
+			oldPoint = new Point(0, screenY);
 
-        private static void ShowImageInWindow(Bitmap image)
-        {
-            var form = new Form
-            {
-                ClientSize = new Size(WindowSize, WindowSize)
-            };
-            form.Controls.Add(new PictureBox { Image = image, Dock = DockStyle.Fill, SizeMode = PictureBoxSizeMode.CenterImage });
-            form.ShowDialog();
-        }
+			for (screenX = 0; screenX < WindowSize; screenX++)
+			{
+				x = (XFrom + screenX * (xTo - XFrom) / WindowSize);
+				y = F(x);
+				if (double.IsNaN(y))
+					continue;
+				screenY = (int)((maxY - y) * WindowSize / (maxY - minY));
+				nextPoint = new Point(screenX, screenY);
+				g.DrawLine(pen, oldPoint, nextPoint);
+				//image.SetPixel(nextPoint.X, nextPoint.Y, Color.Blue);
+				oldPoint = nextPoint;
+			}
+		}
 
-        static void Main(string[] args)
-        {
-            Bitmap image = new Bitmap(WindowSize, WindowSize);
-            Graphics g = Graphics.FromImage(image);
-            g.FillRectangle(Brushes.Azure, 0, 0, image.Width, image.Height);
+		private static void ShowImageInWindow(Bitmap image)
+		{
+			var form = new Form
+			{
+				ClientSize = new Size(WindowSize, WindowSize)
+			};
+			form.Controls.Add(new PictureBox { Image = image, Dock = DockStyle.Fill, SizeMode = PictureBoxSizeMode.CenterImage });
+			form.ShowDialog();
+		}
 
-            Tuple<double, double> minMax = GetMinMaxValues(myF, XFrom, XTo);
+		static void Main(string[] args)
+		{
+			Bitmap image = new Bitmap(WindowSize + 1, WindowSize + 1);
+			g = Graphics.FromImage(image);
+			g.FillRectangle(Brushes.Azure, 0, 0, image.Width, image.Height);
 
-            CreateImage(image, myF, XFrom, XTo);
+			CreateImage(image, myF, XFrom, XTo);
 
-            // image.Save("img.png", ImageFormat.Png);
+			// image.Save("img.png", ImageFormat.Png);
 
-            ShowImageInWindow(image);
+			ShowImageInWindow(image);
 
-        }
-    }
+		}
+	}
 }
